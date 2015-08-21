a5cd6b524495462f34fd9ea1d881c7b04b944455
mario <mario@mate.bme.northwestern.edu>
2015-08-21 17:08:33 -0500

    fixed PBC.f90

--- PBC.f90
+++ PBC.f90
@@ -13,13 +13,13 @@ end function
 
 real function PBCSYMR(i,dimi) ! returns the PBC cell coordinate 
 real*8 i, dimi
-PBCSYMR = mod(i-1.0+5.0*dimi, dimi) + 1.0
+PBCSYMR = mod(i+5.0*dimi,dimi)
 end function
 
 real function PBCREFR(i,dimi) ! returns the reflection cell coordinate 
 real*8 i, dimi, iaux
 integer iaux2
-iaux = mod(i-1.0+5.0*dimi,dimi)+1.0
-iaux2 = mod(int((i-1.0)/dimi),2)
-PBCREFR = iaux+(dimi-2.0*iaux+1.0)*float(iaux2)
+iaux = mod(i+5.0*dimi,dimi)
+iaux2 = mod(int(i/dimi),2)
+PBCREFR = iaux+(dimi-2.0*iaux)*float(iaux2)
 end function
