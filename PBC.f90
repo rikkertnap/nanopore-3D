407ecb422a531c707683affa5db5e350a7c488e0
mario <mario@mate.bme.northwestern.edu>
2015-08-21 21:38:27 -0500

    stupid MOD.

--- PBC.f90
+++ PBC.f90
@@ -6,7 +6,7 @@ end function
 integer function PBCREFI(i,dimi) ! returns the reflection cell coordinate 
 integer i, dimi, iaux, iaux2
 iaux = mod(i-1+5*dimi,dimi)+1
-iaux2 = mod(int(float(i-1)/float(dimi)),2)
+iaux2 = abs(mod(int(float(i-1)/float(dimi)),2))
 PBCREFI = iaux+(dimi-2*iaux+1)*iaux2
 end function
 
@@ -20,6 +20,6 @@ real function PBCREFR(i,dimi) ! returns the reflection cell coordinate
 real*8 i, dimi, iaux
 integer iaux2
 iaux = mod(i+5.0*dimi,dimi)
-iaux2 = mod(floor(i/dimi),2)
+iaux2 = abs(mod(floor(i/dimi),2))
 PBCREFR = iaux+(dimi-2.0*iaux)*float(iaux2)
 end function
