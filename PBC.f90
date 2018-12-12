325e1dbde84ce47b4949dc7eb9a6e2a44530d495
mario <m_taglia@hotmail.com>
2018-12-11 23:16:29 -0300

    fixed error in PBC for small dim

--- PBC.f90
+++ PBC.f90
@@ -1,25 +1,29 @@
 integer function PBCSYMI(i,dimi) ! returns the PBC cell coordinate 
-integer i, dimi
-PBCSYMI = mod(i-1+5*dimi, dimi) + 1
+integer i, dimi, p
+p = abs(i) + 10
+PBCSYMI = mod(i-1+p*dimi, dimi) + 1
 end function
 
 integer function PBCREFI(i,dimi) ! returns the reflection cell coordinate 
-integer i, dimi, iaux, iaux2
-iaux = mod(i-1+5*dimi,dimi)+1
+integer i, dimi, iaux, iaux2, p
+p = abs(i) + 10
+iaux = mod(i-1+p*dimi,dimi)+1
 iaux2 = abs(mod(floor(float(i-1)/float(dimi)),2))
 PBCREFI = iaux+(dimi-2*iaux+1)*iaux2
 end function
 
 
 real function PBCSYMR(i,dimi) ! returns the PBC cell coordinate 
-real*8 i, dimi
-PBCSYMR = mod(i+5.0*dimi,dimi)
+real*8 i, dimi, p
+p = abs(i) +  10.0
+PBCSYMR = mod(i+p*dimi,dimi)
 end function
 
 real function PBCREFR(i,dimi) ! returns the reflection cell coordinate 
-real*8 i, dimi, iaux
+real*8 i, dimi, iaux, p
 integer iaux2
-iaux = mod(i+5.0*dimi,dimi)
+p = abs(i) + 10.0
+iaux = mod(i+p*dimi,dimi)
 iaux2 = abs(mod(floor(i/dimi),2))
 PBCREFR = iaux+(dimi-2.0*iaux)*float(iaux2)
 end function
